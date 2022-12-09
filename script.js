3c23a7cad34204294641161547bf501cf6b4c1f3
Craig Maart <craigmaart@gmail.com>
2022-12-09 10:05:04 +0200

    Completed Calculator app without Updates

---
+++ script.js
@@ -0,0 +1,28 @@
+let calculator_input_Number_V = document.getElementById(
+  "calculator_input_Number"
+);
+let calculator_input_Number_Previews_V = document.getElementById(
+  "calculator_input_Number_Previews"
+);
+
+let Calculator_input_AC_V = document.getElementById("Calculator_input_AC");
+
+let DisplayValue;
+
+//This function is for Displaying Click Events
+function display(DisplayValue) {
+  calculator_input_Number_V.value += DisplayValue;
+  console.log(calculator_input_Number_V.value);
+}
+
+Calculator_input_AC_V.addEventListener("click", () => {
+  calculator_input_Number_V.value = "";
+  calculator_input_Number_Previews_V.value = "";
+});
+
+function displaySolve() {
+  let eq = calculator_input_Number_V.value;
+  calculator_input_Number_Previews_V.value = eq;
+  let y = eval(eq);
+  calculator_input_Number_V.value = y;
+}
